96770e1c9fc66df2f9b5e479098de0314cb00a8a
Phạm Lợi <phamloi@Phams-MacBook-Pro-2.local>
2023-06-02 01:43:02 +0700

    “Push”

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getDataProduct } from '../api/dataDrawFilter';
 import '../assets/less/AllProducts.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AllProducts = () => {
     const [products, setProducts] = useState([]);
@@ -78,71 +79,140 @@ const AllProducts = () => {
     const allCategories = ['all', 'smartphones', 'tablets'];
     const allBrands = [...new Set(products.map((product) => product.brand))];
 
+    const handleFilterBrandCategory = (brand, category) => {
+        if (brand === 'all' && category === 'all') {
+            setFilteredProducts(products);
+        } else if (brand === 'all') {
+            const filtered = products.filter((product) => product.category === category);
+            setFilteredProducts(filtered);
+        } else if (category === 'all') {
+            const filtered = products.filter((product) => product.brand === brand);
+            setFilteredProducts(filtered);
+        } else {
+            const filtered = products.filter(
+                (product) => product.brand === brand && product.category === category
+            );
+            setFilteredProducts(filtered);
+        }
+    };
+
+    const filterButtons = (
+        <div className="filters">
+            {/* Xóa dòng này */}
+            {/* <button onClick={() => handleFilterBrandCategory('all', 'all')} className="btn btn-secondary">
+        Tất cả sản phẩm
+      </button> */}
+            {allCategories.map((category) => (
+                <button
+                    key={category}
+                    onClick={() => handleFilterBrandCategory('all', category)}
+                    className="btn btn-secondary"
+                >
+                    {category}
+                </button>
+            ))}
+        </div>
+    );
+
+    const filteredBrandProducts = [...new Set(filteredProducts.map((product) => product.brand))];
+    const filteredCategoryProducts = [
+        ...new Set(filteredProducts.map((product) => product.category)),
+    ];
+
+    const brandButtons = (
+        <div className="filters">
+            {/* Xóa dòng này */}
+            {/* <button onClick={() => handleFilterBrandCategory('all', 'all')} className="btn btn-secondary">
+        All Brand
+      </button> */}
+            {filteredBrandProducts.map((brand) => (
+                <button
+                    key={brand}
+                    onClick={() => handleFilterBrandCategory(brand, 'all')}
+                    className="btn btn-secondary"
+                >
+                    {brand}
+                </button>
+            ))}
+        </div>
+    );
+
     return (
         <div className="container">
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="id"
-                    value={formData.id}
-                    onChange={handleInputChange}
-                    placeholder="ID"
-                />
-                <input
-                    type="text"
-                    name="title"
-                    value={formData.title}
-                    onChange={handleInputChange}
-                    placeholder="Title"
-                />
-                <input
-                    type="text"
-                    name="price"
-                    value={formData.price}
-                    onChange={handleInputChange}
-                    placeholder="Price"
-                />
-                <input
-                    type="text"
-                    name="brand"
-                    value={formData.brand}
-                    onChange={handleInputChange}
-                    placeholder="Brand"
-                />
-                <input
-                    type="text"
-                    name="category"
-                    value={formData.category}
-                    onChange={handleInputChange}
-                    placeholder="Category"
-                />
-                <input
-                    type="text"
-                    name="thumbnail"
-                    value={formData.thumbnail}
-                    onChange={handleInputChange}
-                    placeholder="Thumbnail"
-                />
-                <button type="submit">Submit</button>
+                <div className="mb-3">
+                    <label className="form-label">ID</label>
+                    <input
+                        type="text"
+                        name="id"
+                        value={formData.id}
+                        onChange={handleInputChange}
+                        placeholder="ID"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Title</label>
+                    <input
+                        type="text"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleInputChange}
+                        placeholder="Title"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Price</label>
+                    <input
+                        type="text"
+                        name="price"
+                        value={formData.price}
+                        onChange={handleInputChange}
+                        placeholder="Price"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Brand</label>
+                    <input
+                        type="text"
+                        name="brand"
+                        value={formData.brand}
+                        onChange={handleInputChange}
+                        placeholder="Brand"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Category</label>
+                    <input
+                        type="text"
+                        name="category"
+                        value={formData.category}
+                        onChange={handleInputChange}
+                        placeholder="Category"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Thumbnail</label>
+                    <input
+                        type="text"
+                        name="thumbnail"
+                        value={formData.thumbnail}
+                        onChange={handleInputChange}
+                        placeholder="Thumbnail"
+                        className="form-control"
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
             </form>
 
-            <div className="filters">
-                <button onClick={() => handleFilterCategory('all')}>Tất cả sản phẩm</button>
-                {allCategories.map((category) => (
-                    <button key={category} onClick={() => handleFilterCategory(category)}>
-                        {category}
-                    </button>
-                ))}
-            </div>
-
-            <div className="filters">
-                <button onClick={() => handleFilterBrand('all')}>All Brand</button>
-                {allBrands.map((brand) => (
-                    <button key={brand} onClick={() => handleFilterBrand(brand)}>
-                        {brand}
-                    </button>
-                ))}
-            </div>
+            {filterButtons}
+            {brandButtons}
 
             <div className="product-list">
                 {filteredProducts.map((product) => (

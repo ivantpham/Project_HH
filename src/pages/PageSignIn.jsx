98b29dda477d026cb14b1b0d8c3b492652b09221
Phạm Lợi <phamloi@Phams-MacBook-Pro-2.local>
2023-06-08 10:58:37 +0700

    “Push”

--- src/pages/PageSignIn.jsx
+++ src/pages/PageSignIn.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-
+import { RiEyeFill, RiEyeOffFill } from 'react-icons/ri';
+import { Alert, Button, Form, FormControl, InputGroup } from 'react-bootstrap';
 import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut,
 } from "firebase/auth";
 import { auth } from "../firebase/fire";
-
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
@@ -17,6 +17,9 @@ function App() {
   const [user, setUser] = useState({});
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [redirectTo, setRedirectTo] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -37,12 +40,17 @@ function App() {
   }, [user, isLoggedIn, redirectTo]);
 
   const login = async () => {
+    setEmailError("");
+    setPasswordError("");
+
+    if (!loginEmail || !loginPassword) {
+      if (!loginEmail) setEmailError("Vui lòng nhập email!");
+      if (!loginPassword) setPasswordError("Vui lòng nhập mật khẩu!");
+      return;
+    }
+
     try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
+      const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
       console.log(user);
       setRedirectTo("/Project_HH"); // Chuyển hướng về trang chủ
     } catch (error) {
@@ -55,44 +63,55 @@ function App() {
     await signOut(auth);
   };
 
-
-  // ________________________________________________________________
-
-
-  // ________________________________________________________________
-
-
   return (
     <div className="App d-flex align-items-center justify-content-center vh-100">
       {!isLoggedIn && (
         <div className="App-login">
           <h3 className="mb-4">Đăng nhập</h3>
-          {loginError && <div className="alert alert-danger">{loginError}</div>}
-
-          <input
-            className="form-control mb-3"
-            placeholder="Email..."
-            value={loginEmail}
-            onChange={(event) => {
-              setLoginEmail(event.target.value);
-            }}
-          />
-          <input
-            className="form-control mb-3"
-            type="password"
-            placeholder="Password..."
-            value={loginPassword}
-            onChange={(event) => {
-              setLoginPassword(event.target.value);
-            }}
-          />
-          <div className="signup-box-text">
-            <div className="signup-text">Bạn chưa có tài khoản???</div>
-            <Link className="signup" to={'/sign-up'} >
-              <a href="" className="signup-link">Đăng Ký Ngay</a>
-            </Link>
-          </div>
-          <button className="btn btn-primary" onClick={login}>Đăng nhập</button>
+          {loginError && <Alert variant="danger">{loginError}</Alert>}
+          <Form>
+            <Form.Group className="mb-3" controlId="loginEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Email..."
+                value={loginEmail}
+                onChange={(event) => setLoginEmail(event.target.value)}
+                isInvalid={emailError !== ""}
+              />
+              <Form.Control.Feedback type="invalid">
+                {emailError}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="loginPassword">
+              <Form.Label>Password</Form.Label>
+              <InputGroup>
+                <Form.Control
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password..."
+                  value={loginPassword}
+                  onChange={(event) => setLoginPassword(event.target.value)}
+                  isInvalid={passwordError !== ""}
+                />
+                <InputGroup.Text
+                  onClick={() => setShowPassword(!showPassword)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
+                </InputGroup.Text>
+                <Form.Control.Feedback type="invalid">
+                  {passwordError}
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+            <div className="signup-box-text">
+              <div className="signup-text">Bạn chưa có tài khoản???</div>
+              <Link className="signup" to={'/sign-up'}>
+                <a href="" className="signup-link">Đăng Ký Ngay</a>
+              </Link>
+            </div>
+            <Button variant="primary" onClick={login}>Đăng nhập</Button>
+          </Form>
         </div>
       )}
 
@@ -100,11 +119,10 @@ function App() {
         <>
           <h4>Đăng nhập thành công:</h4>
           {user?.email}
-          <button className="btn btn-danger" onClick={logout}>Đăng xuất</button>
+          <Button variant="danger" onClick={logout}>Đăng xuất</Button>
         </>
       )}
     </div>
-
   );
 }
 
--- src/pages/PageSignUp.jsx
+++ src/pages/PageSignUp.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 import {
   createUserWithEmailAndPassword,
@@ -9,19 +11,25 @@ import {
 } from "firebase/auth";
 import { auth } from "../firebase/fire";
 
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [registerPasswordConfirm, setRegisterPasswordConfirm] = useState("");
   const [registerError, setRegisterError] = useState("");
+  const [registerEmptyError, setRegisterEmptyError] = useState(false);
+  const [passwordLengthError, setPasswordLengthError] = useState(false);
+  const [invalidEmailError, setInvalidEmailError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
 
   const [user, setUser] = useState({});
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Thêm biến isLoggedIn
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [redirectTimer, setRedirectTimer] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -32,18 +40,57 @@ function App() {
   }, []);
 
   useEffect(() => {
-    setIsLoggedIn(!!user?.email); // Cập nhật giá trị của isLoggedIn khi user.email thay đổi
-  }, [user]);
+    setIsLoggedIn(!!user?.email);
+
+    if (isLoggedIn) {
+      const timer = setTimeout(() => {
+        setRedirectTimer(true); // Bắt đầu đếm thời gian chuyển hướng
+      }, 2000);
+
+      return () => clearTimeout(timer); // Hủy đếm thời gian nếu component bị unmount
+    }
+  }, [user, isLoggedIn]);
+
+  useEffect(() => {
+    if (redirectTimer) {
+      const timer = setTimeout(() => {
+        // Chuyển hướng đến "/Project_HH" sau khi đếm đủ 1 giây
+        window.location.href = "/Project_HH";
+      }, 1000);
+
+      return () => clearTimeout(timer); // Hủy đếm thời gian nếu component bị unmount
+    }
+  }, [redirectTimer]);
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
   const register = async () => {
     try {
       if (registerPassword === registerPasswordConfirm) {
-        const user = await createUserWithEmailAndPassword(
-          auth,
-          registerEmail,
-          registerPassword
-        );
-        console.log(user);
+        if (registerEmail && registerPassword && registerPasswordConfirm) {
+          if (registerPassword.length >= 6) {
+            if (validateEmail(registerEmail)) {
+              const user = await createUserWithEmailAndPassword(
+                auth,
+                registerEmail,
+                registerPassword
+              );
+              console.log(user);
+            } else {
+              setInvalidEmailError(true);
+              setRegisterError("");
+            }
+          } else {
+            setPasswordLengthError(true);
+            setRegisterError("");
+          }
+        } else {
+          setRegisterEmptyError(true);
+          setRegisterError("");
+        }
       } else {
         setRegisterError("Mật Khẩu Không Trùng Khớp!");
 
@@ -72,14 +119,37 @@ function App() {
     await signOut(auth);
   };
 
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
 
+  const toggleShowConfirmPassword = () => {
+    setShowConfirmPassword((prevState) => !prevState);
+  };
 
   return (
     <div className="App d-flex align-items-center justify-content-center vh-100">
       {!isLoggedIn && (
         <div className="App-sign-up">
           <h3 className="mb-3"> Tạo Tài Khoản </h3>
-          {registerError && <div className="alert alert-danger">Mật Khẩu Không Trùng Khớp!</div>}
+          {registerError && (
+            <div className="alert alert-danger">{registerError}</div>
+          )}
+          {registerEmptyError && (
+            <div className="alert alert-danger">
+              Vui lòng điền đầy đủ thông tin!
+            </div>
+          )}
+          {passwordLengthError && (
+            <div className="alert alert-danger">
+              Mật khẩu phải có ít nhất 6 ký tự!
+            </div>
+          )}
+          {invalidEmailError && (
+            <div className="alert alert-danger">
+              Thông tin bạn nhập không phải là email
+            </div>
+          )}
 
           <input
             className="form-control mb-2"
@@ -87,31 +157,53 @@ function App() {
             value={registerEmail}
             onChange={(event) => {
               setRegisterEmail(event.target.value);
+              setInvalidEmailError(false);
             }}
           />
-          <input
-            className="form-control mb-2"
-            placeholder="Password..."
-            value={registerPassword}
-            onChange={(event) => {
-              setRegisterPassword(event.target.value);
-            }}
-          />
-          <input
-            className="form-control mb-2 pwcf"
-            placeholder="Password Confirm..."
-            value={registerPasswordConfirm}
-            onChange={(event) => {
-              setRegisterPasswordConfirm(event.target.value);
-            }}
-          />
+          <div className="password-wrapper">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="form-control mb-2"
+              placeholder="Password..."
+              value={registerPassword}
+              onChange={(event) => {
+                setRegisterPassword(event.target.value);
+                setPasswordLengthError(false);
+              }}
+            />
+            <FontAwesomeIcon
+              icon={showPassword ? faEyeSlash : faEye}
+              className="password-icon"
+              onClick={toggleShowPassword}
+            />
+          </div>
+          <div className="password-wrapper">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              className="form-control mb-2 pwcf"
+              placeholder="Password Confirm..."
+              value={registerPasswordConfirm}
+              onChange={(event) => {
+                setRegisterPasswordConfirm(event.target.value);
+              }}
+            />
+            <FontAwesomeIcon
+              icon={showConfirmPassword ? faEyeSlash : faEye}
+              className="password-icon"
+              onClick={toggleShowConfirmPassword}
+            />
+          </div>
           <div className="login-box-text">
             <div className="login-text">Bạn đã có tài khoản???</div>
-            <Link className="login" to={'/sign-in'} >
-              <a href="" className="login-link">Đăng Nhập Ngay</a>
+            <Link className="login" to={"/sign-in"}>
+              <a href="" className="login-link">
+                Đăng Nhập Ngay
+              </a>
             </Link>
           </div>
-          <button className="btn btn-primary" onClick={register}> Create User</button>
+          <button className="btn btn-primary" onClick={register}>
+            Create User
+          </button>
         </div>
       )}
 
@@ -119,11 +211,12 @@ function App() {
         <>
           <h4> Xin Chào Mừng: </h4>
           {user?.email}
-          <button className="btn btn-danger" onClick={logout}> Sign Out </button>
+          <button className="btn btn-danger" onClick={logout}>
+            Sign Out
+          </button>
         </>
       )}
     </div>
-
   );
 }
 
